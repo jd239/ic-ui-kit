167c86f7c2f1f77b1df0d2eb344aed1dcb173395
jd239 <116016939+jd239@users.noreply.github.com>
2024-01-19 14:58:26 +0000

    feat(web-components): updated styling for button and input component container
    - Added large size for input container
    - Added calendar button styling

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -819,7 +819,7 @@ export namespace Components {
         /**
           * The size of the input component container component.
          */
-        "size"?: IcSizesNoLarge;
+        "size"?: IcSizes;
         /**
           * @deprecated This prop should not be used anymore. Set prop `size` to "small" instead.
          */
@@ -4002,7 +4002,7 @@ declare namespace LocalJSX {
         /**
           * The size of the input component container component.
          */
-        "size"?: IcSizesNoLarge;
+        "size"?: IcSizes;
         /**
           * @deprecated This prop should not be used anymore. Set prop `size` to "small" instead.
          */
