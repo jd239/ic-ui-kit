fb1675c61f175a4f9cb223831ff5a77cb861544c
jd239 <116016939+jd239@users.noreply.github.com>
2024-02-14 12:16:42 +0000

    feat(canary-react): added date input documentation
    Added date input documentation in storybook. Updated storybook config to display getting started
    page on load

--- packages/canary-react/src/component-tests/IcDateInput/IcDateInput.cy.tsx
+++ packages/canary-react/src/component-tests/IcDateInput/IcDateInput.cy.tsx
@@ -14,15 +14,26 @@ const ARROW_UP_KEY = "{upArrow}";
 const ARROW_DOWN_KEY = "{downArrow}";
 const DAY_INPUT_ARIA_LABEL = 'input[aria-label="day"]';
 const MONTH_INPUT_ARIA_LABEL = 'input[aria-label="month"]';
+const CUSTOM_DISABLE_DAY_MESSAGE = "Date disabled";
+const STATUS_TEXT_SPAN = ".statustext";
 // TODO: Add tests which involve checking customEvent returned object
 
-describe("IcDataTables", () => {
+describe("IcDateInput", () => {
+  beforeEach(() => {
+    cy.injectAxe();
+  });
+  afterEach(() => {
+    cy.task("generateReport");
+  });
+
   it("renders", () => {
     mount(<IcDateInput label="Test Label" />);
 
     cy.checkHydrated(DATE_INPUT);
 
     cy.get(DATE_INPUT).should("to.be.visible");
+
+    cy.checkA11yWithWait();
   });
 
   it("should update value on ArrowUp on day input", () => {
@@ -47,6 +58,42 @@ describe("IcDataTables", () => {
     cy.findShadowEl(DATE_INPUT, DAY_INPUT_CLASS).type("/");
 
     cy.findShadowEl(DATE_INPUT, DAY_INPUT_CLASS).should("have.value", "30");
+  });
+
+  it("should display validtion error if date in the past is entered while disable past date is set to true", () => {
+    mount(<IcDateInput label="Test Label" disablePast />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("01");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("01");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2000");
+
+    cy.get("ic-date-input")
+      .shadow()
+      .find(".statustext")
+      .should(
+        "have.text",
+        "Dates in the past are not allowed. Please select a date in the future."
+      );
+  });
+
+  it("should display validtion error if date in the past is entered while disable past date is set to true", () => {
+    mount(<IcDateInput label="Test Label" disableFuture />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("01");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("01");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("4000");
+
+    cy.get("ic-date-input")
+      .shadow()
+      .find(".statustext")
+      .should(
+        "have.text",
+        "Dates in the future are not allowed. Please select a date in the past."
+      );
   });
 
   it("should remove validation status (from now) when disabled date is updated", () => {
@@ -179,7 +226,7 @@ describe("IcDataTables", () => {
       "2025"
     );
 
-    cy.get("ic-button").click();
+    cy.get("button").click();
 
     cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).should("have.value", "");
     cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should(
@@ -207,7 +254,7 @@ describe("IcDataTables", () => {
       "2025"
     );
 
-    cy.get("ic-button").click();
+    cy.get("button").click();
 
     cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).should("have.value", "");
     cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should(
@@ -217,8 +264,7 @@ describe("IcDataTables", () => {
     cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).should("have.value", "");
   });
 
-  it.skip("should test setting date to null programatically", () => {
-    // TODO: Add null as a valid value type
+  it("should test setting date to null programatically", () => {
     mount(<DateInputUpdateValue updatedValue={null} />);
 
     cy.checkHydrated(DATE_INPUT);
@@ -236,7 +282,7 @@ describe("IcDataTables", () => {
       "2025"
     );
 
-    cy.get("ic-button").click();
+    cy.get("button").click();
 
     cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).should("have.value", "");
     cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should(
@@ -244,5 +290,228 @@ describe("IcDataTables", () => {
       ""
     );
     cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).should("have.value", "");
+  });
+
+  it("should enter complete date and check for accessibility", () => {
+    mount(<IcDateInput label="Test Label" />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("30");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("04");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2000");
+
+    cy.checkA11yWithWait();
+  });
+
+  it("should populate screen reader aria live when date is complete and updated", () => {
+    mount(<IcDateInput label="Test Label" />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("30");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("04");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2000");
+
+    cy.get("ic-date-input")
+      .shadow()
+      .find("span[role='status']")
+      .should("have.text", "Selected date: Sunday, 30 April 2000");
+
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type(
+      "{Backspace}{Backspace}{Backspace}{Backspace}"
+    );
+
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2024");
+
+    cy.get("ic-date-input")
+      .shadow()
+      .find("span[role='status']")
+      .should("have.text", "Selected date: Tuesday, 30 April 2024");
+  });
+
+  it("should display validation message if date before min with date string with slashes", () => {
+    mount(<IcDateInput label="Test Label" min="01/07/2001" />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("01");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("06");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2000");
+
+    cy.get("ic-date-input")
+      .shadow()
+      .find(".statustext")
+      .should("have.text", "Please enter a date after 01/07/2001.");
+  });
+
+  it("should display validation message if date before max with date string with slashes", () => {
+    mount(<IcDateInput label="Test Label" max="01/07/2024" />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("01");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("12");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2050");
+
+    cy.findShadowEl(DATE_INPUT, STATUS_TEXT_SPAN).should(
+      "have.text",
+      "Please enter a date before 01/07/2024."
+    );
+  });
+
+  it("should move the cursor to the next input when using slash. Day > Month", () => {
+    mount(<IcDateInput label="Test Label" />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("/");
+
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should("have.focus");
+  });
+
+  it("should move the cursor to the next input when using dash. Day > Month", () => {
+    mount(<IcDateInput label="Test Label" />);
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("-");
+
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should("have.focus");
+  });
+
+  it("should display custom disabledDaysMessage if disabled day is set", () => {
+    mount(
+      <IcDateInput
+        label="Test Label"
+        disableDaysMessage={CUSTOM_DISABLE_DAY_MESSAGE}
+        disableDays={[0]}
+      />
+    );
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("18");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("02");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("2024");
+
+    cy.findShadowEl(DATE_INPUT, STATUS_TEXT_SPAN).should(
+      "have.text",
+      CUSTOM_DISABLE_DAY_MESSAGE
+    );
+  });
+
+  it("should display custom disabledFutureMessage if disabled future day is set", () => {
+    mount(
+      <IcDateInput
+        label="Test Label"
+        disableFutureMessage={CUSTOM_DISABLE_DAY_MESSAGE}
+        disableFuture
+      />
+    );
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("18");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("02");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("3024");
+
+    cy.findShadowEl(DATE_INPUT, STATUS_TEXT_SPAN).should(
+      "have.text",
+      CUSTOM_DISABLE_DAY_MESSAGE
+    );
+  });
+
+  it("should display custom disabledPastMessage if disabled past day is set", () => {
+    mount(
+      <IcDateInput
+        label="Test Label"
+        disablePastMessage={CUSTOM_DISABLE_DAY_MESSAGE}
+        disablePast
+      />
+    );
+
+    cy.checkHydrated(DATE_INPUT);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type("18");
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type("02");
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type("1990");
+
+    cy.findShadowEl(DATE_INPUT, STATUS_TEXT_SPAN).should(
+      "have.text",
+      CUSTOM_DISABLE_DAY_MESSAGE
+    );
+  });
+
+  it("should increase day if value already set", () => {
+    mount(<IcDateInput label="Test Label" value="20/01/2000" />);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).focus();
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type(ARROW_UP_KEY);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).should(
+      "have.value",
+      "21"
+    );
+  });
+
+  it("should decrease day if value already set", () => {
+    mount(<IcDateInput label="Test Label" value="20/01/2000" />);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).focus();
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).type(ARROW_DOWN_KEY);
+
+    cy.findShadowEl(DATE_INPUT, DAY_INPUT_ARIA_LABEL).should(
+      "have.value",
+      "19"
+    );
+  });
+
+  it("should increase month if value already set", () => {
+    mount(<IcDateInput label="Test Label" value="20/01/2000" />);
+
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).focus();
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type(ARROW_UP_KEY);
+
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should(
+      "have.value",
+      "02"
+    );
+  });
+
+  it("should decrease month if value already set", () => {
+    mount(<IcDateInput label="Test Label" value="20/01/2000" />);
+
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).focus();
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).type(ARROW_DOWN_KEY);
+
+    cy.findShadowEl(DATE_INPUT, MONTH_INPUT_ARIA_LABEL).should(
+      "have.value",
+      "12"
+    );
+  });
+
+  it("should increase year if value already set", () => {
+    mount(<IcDateInput label="Test Label" value="20/01/2000" />);
+
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).focus();
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type(ARROW_UP_KEY);
+
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).should(
+      "have.value",
+      "2001"
+    );
+  });
+
+  it("should decrease year if value already set", () => {
+    mount(<IcDateInput label="Test Label" value="20/01/2000" />);
+
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).focus();
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).type(ARROW_DOWN_KEY);
+
+    cy.findShadowEl(DATE_INPUT, YEAR_INPUT_ARIA_LABEL).should(
+      "have.value",
+      "1999"
+    );
   });
 });
--- packages/canary-react/src/component-tests/IcDateInput/IcDateInputTestData.tsx
+++ packages/canary-react/src/component-tests/IcDateInput/IcDateInputTestData.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import { IcButton } from "@ukic/react";
 import { IcDateInput } from "../../components";
 
 type updateValueProp = {
-  updatedValue: string | undefined;
+  updatedValue: string | Date | undefined | null;
 };
 
 export const DateInputUpdateValue = ({ updatedValue }: updateValueProp) => {
-  const [value, setValue] = useState<string | undefined>("31/08/2025");
+  const [value, setValue] = useState<string | undefined | null | Date>(
+    "31/08/2025"
+  );
   const handleClick = () => {
     setValue(updatedValue);
   };
   return (
     <>
       <IcDateInput label="Test label" value={value} />
-      <IcButton onClick={handleClick}>Clear</IcButton>
+      <button onClick={handleClick}>Clear</button>
     </>
   );
 };
